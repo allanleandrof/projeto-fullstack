b6c9b74ecb7ee6f6aaa2de06457a179f800f8331
allanleandrof <alanleandrof@gmail.com>
2025-07-05 13:51:15 -0300

    feat: add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import FornecedorForm from './pages/Fornecedor/FornecedorForm'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Inicial from './pages/Inicial'
+import Menu from './components/Menu'
 
 const App = () => {
   return (
-    <>
-    { /* Por enquanto, sem roteamento */ }
-      <FornecedorForm />
-    </>
+    <BrowserRouter>
+    <Menu />
+      <Routes>
+        <Route path="/" element={<Inicial />} />
+        <Route path="/cadastrar-fornecedor" element={<FornecedorForm />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Inicial.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Inicial = () => {
+  return (
+    <div>Aqui sera a página inicial</div>
+  )
+}
+
+export default Inicial
