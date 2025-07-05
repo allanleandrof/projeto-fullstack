1bf78be6d012fdd0796bc199a7adef93fa6800e5
allanleandrof <alanleandrof@gmail.com>
2025-07-05 14:20:13 -0300

    fix: button add fornecedor

--- frontend/src/pages/Fornecedor/FornecedorList.jsx
+++ frontend/src/pages/Fornecedor/FornecedorList.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Button, Container, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import { FaPlus, FaQuestionCircle } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const FornecedorList = () => {
   return (
-    <div>FornecedorList</div>
+    <div>
+      <Container className="mt-5">
+        <h2 className="mb-4 d-flex align-items-center">
+          Lista de Fornecedores
+          <OverlayTrigger
+                placement="right"
+                overlay={<Tooltip>Visualize, edite ou exclua fornecedores</Tooltip>}
+            >
+                <span className="ms-2" style={{ cursor: 'pointer' }}>
+                    <FaQuestionCircle />
+                </span>
+            </OverlayTrigger>
+        </h2>
+
+        <div className="mb-3">
+          <Button as={Link} to='/cadastrar-fornecedor' variant='primary'>
+            <FaPlus className='me-2' /> Adicionar Fornecedor
+          </Button>
+        </div>
+      </Container>
+    </div>
   )
 }
 
